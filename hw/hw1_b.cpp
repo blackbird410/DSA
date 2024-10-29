0a96fb890b7903a8beac6b07337e639d81f08528
Neil Taison Rigaud <ntsrigaud14@gmail.com>
2024-10-29 10:25:27 +0800

    add: hw b class for storing board values

--- hw/hw1_b.cpp
+++ hw/hw1_b.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-// TODO: Research how is Sudoku game played
 
 // Write a recursive program to solve the 4x4 Sudoku game.
 // The program should be able to solve the following puzzle:
@@ -20,22 +19,80 @@
 // 4 0 0 1
 
 #define SIZE 4
+#define SPACE 0
+#define SOLVED 1
+
+using namespace std;
 
 class SudokuBoard {
 public:
-  SudokuBoard();
-  SudokuBoard(SudokuBoard &&) = default;
-  SudokuBoard(const SudokuBoard &) = default;
-  SudokuBoard &operator=(SudokuBoard &&) = default;
-  SudokuBoard &operator=(const SudokuBoard &) = default;
-  ~SudokuBoard();
+  SudokuBoard() {};
+  ~SudokuBoard() {};
+
+  void setCell(int row, int col, int data) {
+    if (row >= SIZE || col >= SIZE || data > SIZE) return;
+    board[row][col] = data;
+  }
+  
+  int getCell(int row, int col) const {
+    return (row >= SIZE || col >= SIZE) ? SPACE : board[row][col];
+  }
+
+  void setState(int value) { state = value; }
+  int getState() const { return state; };
+  
+  int isValid(int value) const {
+    return (value <= SIZE && value > 0);
+  }
+
+  void fillBoard() {
+    int i, j, temp;
+    cout << "Entering board values..." << endl;
+    for (i = 0; i < SIZE; ++i) {
+      for (j = 0; j < SIZE; ++j) {
+        temp = SIZE + 1;
+        while (!isValid(temp) || temp == SPACE ) {
+          cout << "BOARD[" << i << "][" << j << "] : ";
+          cin >> temp;
+          cout << temp << endl;
+        }
+        board[i][j] = temp;
+      }
+    }
+  };
+
+  int isSolved() const {
+    int i, j;
+    for (i = 0; i < SIZE; ++i)
+      for (j = 0; j < SIZE; ++j)
+        if (board[i][j] == SPACE) 
+          return !SOLVED;
+  };
+
+  friend ostream& operator<<(ostream& out, SudokuBoard& s) {
+    int i, j;
+    
+    for (i = 0; i < SIZE; ++i) {
+      for (j = 0; j < SIZE; ++j) {
+        out << s.board[i][j];
+        if (j < SIZE - 1) cout << " ";
+      }
+      if (i < SIZE - 1) cout << endl;
+    }
+
+    return out;
+  };
 
 private:
-  int board[4][4];
+  int board[SIZE][SIZE];
   int state;
 };
 
-SudokuBoard::SudokuBoard() : state{0} {}
+int main (int argc, char *argv[]) {
+  SudokuBoard b;
+  b.fillBoard();
+  cout << b << endl;
 
-SudokuBoard::~SudokuBoard() {
+  return 0;
 }
+
