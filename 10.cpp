6a7a44d07a6ad866f960548e762e635330692135
Neil Taison Rigaud <ntsrigaud14@gmail.com>
2024-10-31 22:39:55 +0800

    add: lab 10 and 11 solutions

---
+++ 10.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+#define PREORDER -1 
+#define POSTORDER 1
+
+using namespace std;
+
+template<class T>
+class TreeInLinkedList {
+private:
+  class TreeNode {
+  private:
+  public:
+    TreeNode(T d, TreeNode *p) : data(d), parent(p) {}
+    TreeNode *parent;
+    T data;
+  };
+  friend ostream& operator<<(ostream& out, TreeNode* p) {
+    out << p->data;
+    return out;
+  }
+  vector<TreeNode *> *nodeList;
+public:
+  TreeInLinkedList() {
+    // Creates an array of pointers to TreeNode objects
+    nodeList = new vector<TreeNode *>();
+  }
+  ~TreeInLinkedList() {
+    nodeList->clear();
+  }
+  void addElement(T data) {
+    // How does it add elements ?
+    // 1 - Check if we are adding 1, because all other elements in the tree will definitely have 1 as a parent ( for all data, 1 | data)
+    int j, k = nodeList->size();
+    if(data == 1) {
+      // 2 - Clear the tree for new values and add 1 as the root of the tree
+      nodeList->clear();
+      nodeList = new vector<TreeNode *>();
+      TreeNode *newNode = new TreeNode(data, nullptr);
+      nodeList->push_back(newNode);
+    }
+    else {
+      // 2 - Check all the preexisting nodes;
+      // If a node value can divide the data, the add a new node for the data to the tree and the node found as its parent
+      // The relation in this tree is that the data in a parent of a node can divide the data in the node
+      for(j = 0;j < k;j ++) {
+        if(data % (*nodeList)[j]->data == 0) {
+          TreeNode *newNode = new TreeNode(data, (*nodeList)[j]);
+          nodeList->push_back(newNode);
+        }
+      }
+    }
+  }
+
+  void traverse(TreeNode *p, int order) {
+    if (!p) return;
+
+    if (order == PREORDER) {
+      cout << p << " ";
+      for (TreeNode* child: *nodeList)
+        if (child->parent == p) 
+          traverse(child, order);
+    } else if (order == POSTORDER) {
+      for (TreeNode* child: *nodeList)
+        if (child->parent == p) 
+          traverse(child, order);
+      cout << p << " ";
+    }
+  }
+
+  void displayPreorder() {
+    if (!nodeList->size()) return;
+
+    traverse(nodeList->at(0), PREORDER);
+  }
+
+  void displayPostorder() {
+    if (!nodeList->size()) return;
+
+    traverse(nodeList->at(0), POSTORDER);
+  }
+};
+
+int main()
+{
+  TreeInLinkedList<int> *tree = new TreeInLinkedList<int>();
+  int j;
+  int n;
+  cin >> n;
+  for(j = 1;j <= n;j ++)
+    tree->addElement(j);
+  tree->displayPreorder();
+  cout << endl;
+  tree->displayPostorder();
+  cout << endl;
+}
+
---
+++ 11.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template<class T>
+class BinarySearchTree {
+private:
+  struct Node {
+    T data;
+    Node *left, *right;
+  };
+
+  void inorder(Node *cur) {
+    if(!cur)
+      return;
+    inorder(cur->left);
+    cout << cur->data << " ";
+    inorder(cur->right);
+  }
+
+  Node *root;
+public:
+  BinarySearchTree() : root(nullptr) {}
+
+  void insertElement(Node* n, T data) {
+    Node*  newNode = new Node();
+    newNode->data = data;
+    newNode->left = newNode->right = nullptr;
+
+    if (data < n->data) {
+      if (!n->left)
+        n->left = newNode;
+      else
+        insertElement(n->left, data);
+    }
+    else {
+      if (!n->right)
+        n->right = newNode;
+      else
+        insertElement(n->right, data);
+    } 
+  }
+
+  void insertElement(T d) {
+    if (root == nullptr) {
+      root = new Node();
+      root->data = d;
+      root->left = root->right = nullptr;
+      return;
+    }
+
+    insertElement(root, d);
+  }
+
+  void print() {
+    inorder(root);
+    cout << endl;
+  }
+
+  bool search(Node* n, T d) {
+    if (!n) return false; 
+
+    return (n->data == d) 
+      ? true : (d < n->data) 
+      ? search(n->left, d) 
+      : search(n->right, d);
+  };
+
+  bool search(T d) {
+    return (!root) ? false : search(root, d);
+  }
+
+  int height(Node* n) {
+    if (!n) return 0;
+
+    int heightLeft = height(n->left) + 1;
+    int heightRight = height(n->right) + 1;
+
+    return (heightLeft > heightRight) ? heightLeft : heightRight;
+  };
+
+  int height() {
+    return height(root);
+  }
+
+};
+
+int main() {
+  int data;
+  string command;
+  BinarySearchTree<int> *bst = new BinarySearchTree<int>();
+  while(true) {
+    cout << "Command: ";
+    cin >> command;
+    if(command == "insert") {
+      cin >> data;
+      bst->insertElement(data);
+    }
+    else if(command == "search") {
+      cin >> data;
+      if(bst->search(data))
+        cout << "true" << endl;
+      else
+        cout << "false" << endl;
+    }
+    else if(command == "height") {
+      cout << bst->height() << endl;
+    }
+    else if(command == "print") {
+      bst->print();
+    }
+    else if(command == "exit") {
+      break;
+    }
+  }
+}
+
